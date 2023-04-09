5ba23e0b8c95ed462e1d7f9a86e47314bb7021da
Georgi Gerganov <ggerganov@gmail.com>
2023-04-09 17:42:29 +0300

    misc : swap tilde key on Mac OS

---
+++ swap-tilde.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ref: https://ppolyzos.com/2020/10/09/swap-places-between-tilde-and-section-sign-%C2%A7-key-in-your-macbook-keyboard/
+
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
+
+#hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
