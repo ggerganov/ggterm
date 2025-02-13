5be7e740f4e50ea853f34be04fc8b9972050570c
Georgi Gerganov <ggerganov@gmail.com>
2025-02-13 20:43:56 +0200

    nvim : update tiny-glimmer

--- init.lua
+++ init.lua
@@ -29,7 +29,89 @@ return require('packer').startup(function(use)
     use {
         'rachartier/tiny-glimmer.nvim',
         config = function()
-            require('tiny-glimmer').setup()
+            require('tiny-glimmer').setup({
+                enabled = true,
+
+                -- Disable this if you wants to debug highlighting issues
+                disable_warnings = true,
+
+                default_animation = "fade",
+                refresh_interval_ms = 6,
+
+                overwrite = {
+                    -- Automatically map keys to overwrite operations
+                    -- If set to false, you will need to call the API functions to trigger the animations
+                    -- WARN: You should disable this if you have already mapped these keys
+                    -- 		 or if you want to use the API functions to trigger the animations
+                    auto_map = true,
+
+                    -- For search and paste, you can easily modify the animation to suit your needs
+                    -- For example you can set a table to default_animation with custom parameters:
+                    -- default_animation = {
+                    --     name = "fade",
+                    --
+                    --     settings = {
+                    --         max_duration = 1000,
+                    --         min_duration = 1000,
+                    --
+                    --         from_color = "DiffDelete",
+                    --         to_color = "Normal",
+                    --     },
+                    -- },
+                    -- settings needs to respect the animation you choose settings
+                    search = {
+                        enabled = true,
+                        default_animation = "fade",
+
+                        -- Keys to navigate to the next match
+                        next_mapping = "nzzzv",
+
+                        -- Keys to navigate to the previous match
+                        prev_mapping = "Nzzzv",
+                    },
+                    paste = {
+                        enabled = true,
+                        default_animation = "fade",
+
+                        -- Keys to paste
+                        paste_mapping = "p",
+
+                        -- Keys to paste above the cursor
+                        Paste_mapping = "P",
+                    },
+                    undo = {
+                        enabled = true,
+
+                        default_animation = {
+                            name = "fade",
+
+                            settings = {
+                                from_color = "DiffDelete",
+
+                                max_duration = 500,
+                                min_duration = 500,
+                            },
+                        },
+                        undo_mapping = "u",
+                    },
+                    redo = {
+                        enabled = true,
+
+                        default_animation = {
+                            name = "fade",
+
+                            settings = {
+                                from_color = "DiffAdd",
+
+                                max_duration = 500,
+                                min_duration = 500,
+                            },
+                        },
+
+                        redo_mapping = "<c-r>",
+                    },
+                },
+            })
         end
     }
 
